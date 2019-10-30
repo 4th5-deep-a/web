ea57ae56413513961665d9ce4b178d98ad5eed0e
Junwoo Hwang <no.water.in.the.house@gmail.com>
2019-10-30 13:43:39 +0900

    191030 Django CRUD

--- django/crud/articles/urls.py
+++ django/crud/articles/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('', views.index),
     path('new/', views.new),
     path('create/', views.create),
+    path('<int:pk>/', views.detail),
+    path('<int:pk>/delete/', views.delete),
+    path('<int:pk>/edit/', views.edit),
+    path('<int:pk>/update/', views.update),
 ]
--- django/crud/articles/views.py
+++ django/crud/articles/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Article
 
 # Create your views here.
 def index(request):
-    articles = Article.objects.all() #=> QuerySet ~= List
+    articles = Article.objects.all().order_by('-id') #=> QuerySet ~= List
 
     context = {
         'articles': articles,
@@ -15,18 +15,49 @@ def index(request):
 def new(request):
     return render(request, 'articles/new.html')
 
-def create(request):
+def create(request): # POST 요청을 받음
     # 넘어온 데이터 받기
-    title = request.GET.get('title')
-    content = request.GET.get('content')
+    # 이유 2번 - GET(URL), POST(http body)
+    title = request.POST.get('title')
+    content = request.POST.get('content')
 
     article = Article.objects.create(title=title, content=content)
     # article.title
     # article.content 
 
-    context = {
-        'title': title,
-        'content': content,
-    }
+    return redirect(f'/articles/{article.pk}/')
 
-    return render(request, 'articles/create.html', context)
+
+def detail(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article': article,
+    }
+    return render(request, 'articles/detail.html', context)
+
+def delete(request, pk):
+    article = Article.objects.get(pk=pk)
+    article.delete()
+    return redirect('/articles/')
+
+def edit(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article': article,
+    }
+    return render(request, 'articles/edit.html', context)
+
+def update(request, pk):
+    # 1. pk에 해당하는 article 가져오기
+    article = Article.objects.get(pk=pk)
+
+    # 2. edit로부터 넘어온 데이터 가져오기
+    title = request.POST.get('title')
+    content = request.POST.get('content')
+    
+    # 3. 넘어온 데이터를 article에 새롭게 저장
+    article.title = title
+    article.content = content
+    article.save()
+
+    return redirect(f'/articles/{article.pk}/') # detail 페이지
