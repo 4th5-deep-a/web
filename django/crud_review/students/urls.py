d0b3e05bbc5261a96ba25c11902570fe12462c03
Junwoo Hwang <no.water.in.the.house@gmail.com>
2019-10-31 17:45:57 +0900

    191031 Django CRUD RESTful

--- django/crud_review/students/urls.py
+++ django/crud_review/students/urls.py
@@ -1,12 +1,39 @@
 from django.urls import path
 from . import views
 
+app_name = 'students'
+
 urlpatterns = [
-    path('', views.index),
-    path('new/', views.new),
-    path('create/', views.create),
-    path('<int:pk>/', views.detail),
-    path('<int:pk>/edit/', views.edit),
-    path('<int:pk>/update/', views.update),
-    path('<int:pk>/delete/', views.delete),
+    path('', views.index, name='index'), # GET /students/
+    path('new/', views.new, name='new'), # GET,POST /students/new/
+    # path('create/', views.create, name='create'), # POST /students/create/ (x)
+    path('<int:pk>/', views.detail, name='detail'), # GET /students/1/
+    path('<int:pk>/edit/', views.edit, name='edit'), # GET,POST /students/1/edit/
+    # path('<int:pk>/update/', views.update, name='update'), # POST /students/1/update/ (x)
+    path('<int:pk>/delete/', views.delete, name='delete'), # POST /students/1/delete/
 ]
+
+# URL Name -> 각각의 path
+# path('주소/', views.함수, name='이름')
+# {% url '이름' %} #=> 주소
+# [장점]
+# 1. 주소의 변경이 필요할 때, urls.py에서만 수정해주면 됨
+# 2. 마지막 '/'를 빼먹는 실수를 차단할 수 있음
+
+# App Name - 특정 app의 urls.py 자체
+# {% url 'app_name:path_name' %} #=> 주소!
+
+
+# RESTful
+# 1. 자원(Resource) - URI
+# 2. 행위(Verb) - HTTP Method (GET, POST, ...)
+# 3. 표현(Representations) - 자원 + 행위
+
+# Django는 PUT/PATCH/DELETE 불가능. 따라서...
+# GET  /students/2/edit/ #=> 수정 페이지 보여줌
+# POST /students/2/edit/ #=> 수정 작업 수행
+
+# ex)
+# GET     /users/1  #=> user 1번 가져옴
+# PUT     /users/1  #=> user 1번 수정
+# DELETE  /users/1  #=> user 1번 삭제
--- django/crud_review/students/views.py
+++ django/crud_review/students/views.py
@@ -15,20 +15,36 @@ def index(request):
     return render(request, 'students/index.html', context)
 
 def new(request):
-    return render(request, 'students/new.html')
+    if request.method == 'POST': #=> 'GET', 'POST'
+        # student를 생성함 (def create)
+        # 1. POST 요청으로 넘어온 데이터 가져오기
+        name = request.POST.get('name')
+        age = request.POST.get('age')
+
+        # 2. Model 클래스 사용해서 DB에 저장!
+        student = Student(name=name, age=age)
+        student.save()
+
+        # 3. 생성된 학생의 상세 정보를 보는 페이지로 이동(Detail)
+        return redirect('students:detail', student.pk)
+    else:
+        # new page를 보여주면 됨 (def new)
+        return render(request, 'students/new.html')
+
+    
 
 # POST 요청을 받는 친구
-def create(request):
-    # 1. POST 요청으로 넘어온 데이터 가져오기
-    name = request.POST.get('name')
-    age = request.POST.get('age')
+# def create(request):
+#     # 1. POST 요청으로 넘어온 데이터 가져오기
+#     name = request.POST.get('name')
+#     age = request.POST.get('age')
 
-    # 2. Model 클래스 사용해서 DB에 저장!
-    student = Student(name=name, age=age)
-    student.save()
+#     # 2. Model 클래스 사용해서 DB에 저장!
+#     student = Student(name=name, age=age)
+#     student.save()
 
-    # 3. 생성된 학생의 상세 정보를 보는 페이지로 이동(Detail)
-    return redirect(f'/students/{student.pk}/')
+#     # 3. 생성된 학생의 상세 정보를 보는 페이지로 이동(Detail)
+#     return redirect('students:detail', student.pk)
 
 def detail(request, pk):
     # 1. pk에 해당하는 student를 DB에서 가져오기
@@ -44,41 +60,58 @@ def detail(request, pk):
 
 
 def edit(request, pk):
-    # 1. pk에 해당하는 학생 DB에서 가져오기
-    student = Student.objects.get(pk=pk)
-
-    # 2. context에 저장
-    context = {
-        'student': student,
-    }
-
-    # 3. render하면서 context 넘겨주기
-    return render(request, 'students/edit.html', context)
-
-# POST 요청만을 받음 -> redirect()
-def update(request, pk):
     # 1. pk에 해당하는 student를 DB에서 가져오기
     student = Student.objects.get(pk=pk)
+    if request.method == 'POST':
+        # student를 수정 (def update)
+        # 2. POST 요청을 통해 넘어온 데이터 가져오기
+        name = request.POST.get('name')
+        age = request.POST.get('age')
 
-    # 2. POST 요청을 통해 넘어온 데이터 가져오기
-    name = request.POST.get('name')
-    age = request.POST.get('age')
+        # 3. student 인스턴스의 정보를 변경 & DB에 반영 -> .save()
+        student.name = name
+        student.age = age
+        student.save()
 
-    # 3. student 인스턴스의 정보를 변경 & DB에 반영 -> .save()
-    student.name = name
-    student.age = age
-    student.save()
+        # 4. student 상세 페이지로 이동(Detail)
+        return redirect('students:detail', student.pk)
+    else:
+        # 수정하는 페이지를 보여줌 (def edit)
+        # 2. context에 저장
+        context = {
+            'student': student,
+        }
 
-    # 4. student 상세 페이지로 이동(Detail)
-    return redirect(f'/students/{student.pk}')
+        # 3. render하면서 context 넘겨주기
+        return render(request, 'students/edit.html', context)
+
+# POST 요청만을 받음 -> redirect()
+# def update(request, pk):
+#     # 1. pk에 해당하는 student를 DB에서 가져오기
+#     student = Student.objects.get(pk=pk)
+
+#     # 2. POST 요청을 통해 넘어온 데이터 가져오기
+#     name = request.POST.get('name')
+#     age = request.POST.get('age')
+
+#     # 3. student 인스턴스의 정보를 변경 & DB에 반영 -> .save()
+#     student.name = name
+#     student.age = age
+#     student.save()
+
+#     # 4. student 상세 페이지로 이동(Detail)
+#     return redirect('students:detail', student.pk)
 
 # POST 요청만을 받음 -> redirect()
 def delete(request, pk):
-    # 1. pk에 해당하는 student를 DB에서 가져오기
-    student = Student.objects.get(pk=pk)
+    if request.method == 'POST':
+        # 1. pk에 해당하는 student를 DB에서 가져오기
+        student = Student.objects.get(pk=pk)
 
-    # 2. student 삭제 (DB에서 삭제하기)
-    student.delete()
+        # 2. student 삭제 (DB에서 삭제하기)
+        student.delete()
 
-    # 3. index 페이지로 이동
-    return redirect('/students/')
+        # 3. index 페이지로 이동
+        return redirect('students:index')
+    
+    # return 없음! 오류!
