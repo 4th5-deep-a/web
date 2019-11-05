6277e0d12d89cd1aace54cb20c9e2412a28d98ca
Junwoo Hwang <no.water.in.the.house@gmail.com>
2019-11-05 12:53:29 +0900

    191105 Django Admin

--- django/crud_review/students/admin.py
+++ django/crud_review/students/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Student, Comment
 
-# Register your models here.
+
+class CommentInline(admin.StackedInline):
+    model = Comment
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'age', 'created_at', 'updated_at')
+    inlines = [
+        CommentInline,
+    ]
+
+# class CommentAdmin(admin.ModelAdmin):
+#     list_display = ('id', 'content', 'created_at', 'updated_at')
+
+admin.site.register(Student, StudentAdmin)
+# admin.site.register(Comment, CommentAdmin)
+
+# 관리자 계정 만들기
+# python manage.py createsuperuser
